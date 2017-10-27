bfd1ec8825e7990eac6b3e34e4faedbdc3f0d2ca
JiXunzhen <playagayson@gmail.com>
2017-10-27 23:29:52 +0800

    [SEO] jhword页面生成

--- jhword/url.php
+++ jhword/url.php
@@ -1,37 +1,36 @@
 <?php
 
-$content = file_get_contents("./result.csv");
+ini_set('memory_limit', '2G');
 
+$content = file_get_contents("./kwd-city.csv");
 $rows = explode("\n", $content);
 
-$cnt = 0;
-
-$file = fopen("./url.csv", 'w');
-
+$file = fopen("./urls/url0.txt", 'w');
 $cnt = 0;
 $idx = 0;
-foreach ($rows as $row) {
-    $info = explode("&", $row);
-    if (preg_match("/ObjectId\(([0-9a-zA-Z]+)\)/", $info[1], $m)) {
-        $id = $m[1];
-        $juheword = MJuheKeyword::fetchItemBySid($info[0]);
+$total = 0;
 
-        if ($juheword) {
-            foreach ($juheword->city as $city) {
-                $urlArr = "http://{$city}.baixing.com/" . urlencode("jh_{$info[0]}") . '/';
-                fwrite($file, $urlArr . "\n");
+foreach ($rows as $row) {
+    $info = explode("\t", $row);
+
+    if (count($info) == 2) {
+        $kwd = $info[0];
+        $citys = explode(",", $info[1]);
+        foreach ($citys as $city) {
+            $urlArr = "http://{$city}.baixing.com/" . urlencode("jh_{$kwd}") . '/';
+            fwrite($file, $urlArr . "\n");
+            $cnt++;
+            $total++;
+
+            if ($cnt == 48000) {
+                $cnt = 0;
+                $idx++;
+                echo "{$idx} * 48000 over.\n";
+                fclose($file);
+                $file = fopen("./urls/url{$idx}.txt", 'w');
             }
         }
     }
-
-    $cnt++;
-    if ($cnt == 1000) {
-        $idx++;
-        $cnt = 0;
-        echo "{$idx}000 over.";
-    }
-
-    usleep(100 * 1000);
 }
 
 fclose($file);
